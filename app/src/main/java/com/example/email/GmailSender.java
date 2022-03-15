9988ecd6f81947322fb76fed4d2f09f7d2c00a0e
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2022-03-15 15:18:18 +0300

    Necessary Activities

---
+++ app/src/main/java/com/example/email/GmailSender.java
@@ -0,0 +1,14 @@
+package com.example.email;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GmailSender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmail_sender);
+    }
+}
