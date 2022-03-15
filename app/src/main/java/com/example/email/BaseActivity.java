6838cfa16bb4ae9921efa8621176ef96fa281a04
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2022-03-15 15:33:36 +0300

    Gmailsender

--- app/src/main/java/com/example/email/BaseActivity.java
+++ app/src/main/java/com/example/email/BaseActivity.java
@@ -3,6 +3,8 @@ package com.example.email;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
     }
+
+public void onClick(View view){
+        try {
+            GmailSender sender = new GmailSender("email@gmail.com","password",);
+            sender.sendMail("This is Sublect","Body","em");
+
+        } catch (Exception e){
+            Log.e("send", "error")
+        }
+}
+
 }
