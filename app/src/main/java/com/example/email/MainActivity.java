f4a70343f967de63642a386563dd5425a3dc3fca
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2022-03-15 11:08:58 +0300

    su

--- app/src/main/java/com/example/email/MainActivity.java
+++ app/src/main/java/com/example/email/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button composeButton = findViewById(R.id.compose_btn);
+        composeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+
+            }
+        });
     }
+
+
+    protected void  sendMail(){
+
+        Log.i("Send Mail", "");
+        String [] TO = {""};
+        String [] CC = {""};
+        Intent  emailIntent  = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send Mail"));
+            finish();
+            Log.i("Sending done","");
+        } catch (android.content.ActivityNotFoundException exception){
+            Toast.makeText(MainActivity.this, "No email Client",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
